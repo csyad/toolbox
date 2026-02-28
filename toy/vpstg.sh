62916d695f28b6904e60076bfbf73a1763cce349
starry <csywasde@outlook.com>
2026-02-28 21:34:36 +0800

    Update vpstg.sh

--- toy/vpstg.sh
+++ toy/vpstg.sh
@@ -6,12 +6,10 @@ RESET="\033[0m"
 
 menu() {
     clear
-    echo -e "${GREEN}=== VPSTG监控通知管理 ===${RESET}"
+    echo -e "${GREEN}=== VPS信息通知 ===${RESET}"
     echo -e "${GREEN}1) 系统信息${RESET}"
     echo -e "${GREEN}2) 网卡信息${RESET}"
     echo -e "${GREEN}3) Docker信息${RESET}"
-    echo -e "${GREEN}4) 智能流量监控与限制${RESET}"
-    echo -e "${GREEN}5) VPS遥控器${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
@@ -28,16 +26,6 @@ menu() {
         3)
             echo -e "${GREEN}正在运行Docker信息脚本...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/vpsd.sh)
-            pause
-            ;;
-        4)
-            echo -e "${GREEN}正在运行智能流量监控与限制脚本...${RESET}"
-            bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/toy/traffic.sh)
-            pause
-            ;;
-        5)
-            echo -e "${GREEN}正在运行VPS遥控器...${RESET}"
-            curl -fsSL https://raw.githubusercontent.com/MEILOI/VPS_BOT_X/main/vps_bot-x/install.sh -o install.sh && chmod +x install.sh && bash install.sh
             pause
             ;;
         0)
