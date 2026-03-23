d67dfd2619b8d495775232ae518e78c896d5f812
starry <csywasde@outlook.com>
2026-03-23 10:23:03 +0800

    Update Alpine.sh

--- Alpine/Alpine.sh
+++ Alpine/Alpine.sh
@@ -51,10 +51,12 @@ menu() {
     echo -e "${YELLOW}[09] Docker管理${RESET}"
     echo -e "${YELLOW}[10] Reality${RESET}"
     echo -e "${YELLOW}[11] Hysteria2${RESET}"
-    echo -e "${YELLOW}[12] fsSing-box${RESET}"
-    echo -e "${YELLOW}[13] 3X-UI面板${RESET}"
-    echo -e "${YELLOW}[14] Realm-xwPF${RESET}"
-    echo -e "${YELLOW}[15] 应用商店${RESET}"
+    echo -e "${YELLOW}[12] Xray-Argo${RESET}"
+    echo -e "${YELLOW}[13] F佬Sing-box${RESET}"
+    echo -e "${YELLOW}[14] vless-all-in-one${RESET}"
+    echo -e "${YELLOW}[15] 3X-UI面板${RESET}"
+    echo -e "${YELLOW}[16] Realm-xwPF${RESET}"
+    echo -e "${YELLOW}[17] 应用商店${RESET}"
     echo -e "${GREEN}[88] 更新脚本${RESET}"
     echo -e "${GREEN}[99] 卸载脚本${RESET}"
     echo -e "${YELLOW}[00] 退出${RESET}"
@@ -74,10 +76,12 @@ menu() {
         9) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/apdocker.sh) ;;
         10) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AZAPReality.sh) ;;
         11) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/AZAPHysteria2.sh) ;;
-        12) bash <(wget -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh) ;;
-        13) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/3xuiAlpine.sh) ;;
-        14) wget -qO- https://raw.githubusercontent.com/zywe03/realm-xwPF/main/xwPF.sh | sudo bash -s install ;;
-        15) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Docker/Store.sh) ;;
+        12) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/Xray2go.sh) ;;
+        13) bash <(wget -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh) ;;
+        14) wget -O vless-server.sh https://raw.githubusercontent.com/Zyx0rx/vless-all-in-one/main/vless-server.sh && chmod +x vless-server.sh && ./vless-server.sh ;;
+        15) bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/Alpine/3xuiAlpine.sh) ;;
+        16) wget -qO- https://raw.githubusercontent.com/zywe03/realm-xwPF/main/xwPF.sh | sudo bash -s install ;;
+        17) bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/Docker/Store.sh) ;;
         88)
             echo -e "${YELLOW}🔄 正在更新脚本...${RESET}"
             curl -fsSL -o "$SCRIPT_PATH" "$SCRIPT_URL" || {
